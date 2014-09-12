1caa5d700cfca2625b062a6851ab25a8b64a7e42
Christopher Halbersma <chalbersma@COIN0.COIN0.LAN>
2014-09-12 13:48:10 -0500

    Fixed blockr link

--- p2pool/bitcoin/networks/altcoin.py
+++ p2pool/bitcoin/networks/altcoin.py
@@ -20,9 +20,9 @@ POW_FUNC = data.hash256
 BLOCK_PERIOD = 600 # s
 SYMBOL = 'ATC'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Bitcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Bitcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.altcoin'), 'altcoin.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'https://atc.blockr.io/block/info'
-ADDRESS_EXPLORER_URL_PREFIX = 'https://atc.blockr.io/address/info'
-TX_EXPLORER_URL_PREFIX = 'https://atc.blockr.io/tx/info'
+BLOCK_EXPLORER_URL_PREFIX = 'https://atc.blockr.io/block/info/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://atc.blockr.io/address/info/'
+TX_EXPLORER_URL_PREFIX = 'https://atc.blockr.io/tx/info/'
 SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
 DUMB_SCRYPT_DIFF = 1
 DUST_THRESHOLD = 0.001e8
